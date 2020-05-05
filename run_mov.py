7cddb99df1bb673eb0ec0c864ad402255a801fc3
anyazz <anyazhang01@yahoo.com>
2020-05-05 16:10:29 -0600

    exhaustive tests

--- run_mov.py
+++ run_mov.py
@@ -10,32 +10,24 @@ def run(i):
 
     opinion_attr = "sex"
 
-    X = np.concatenate([np.linspace(0,  80, 17)])
-    n=32
+    X = [0, 3, 6, 9, 15, 20, 30, 45, 60, 80, 100, 120, 150, 200, 300]
+    X=X[:2]
+    n = 32
     Y = [] # FTPL payoff
     BR = [] # BR payoff
-    file_X = open("FTPL_X.txt", "w")
-    file_BR = open("FTPL_BR.txt", "w")
-    file_Y = open("FTPL_Y.txt", "w")
-    file_XA = open("FTPL_XA.txt", "w")
-    file_XB = open("FTPL_XB.txt", "w")
-    XAs, XBs = [], []
+    file_BR = open("data/{}_br.txt".format(i), "w")
+    file_Y = open("data/{}_Y.txt".format(i), "w")
     A = Candidate("A", 0, 1, n)
-    B = Candidate("B", 40, 0, n)
+    B = Candidate("B", 30, 0, n)
     e = Election(data, n, [A, B], 10, opinion_attr, rand=False)
     for x in X:
         print("BUDGET ", x)
-        file_X.write(str(x) + ', ')
         A.k=x
         e.update_network()
 
         ftpl(e, 5, .1, x)
 
         ftpl_mean = e.calculate_mean()
-        XAs.append(A.X)
-        file_XA.write(str(A.X) + ',')
-        XBs.append(B.X)
-        file_XB.write(str(B.X) + ',')
         Y.append(ftpl_mean)
         file_Y.write(str(ftpl_mean)+ ', ')
 
@@ -45,26 +37,25 @@ def run(i):
         BR.append(br_mean)
 
         file_BR.write(str(br_mean) + ', ')
-        for file in [file_X, file_BR, file_Y, file_XA, file_XB]:
+        for file in [file_BR, file_Y]:
             file.flush()
         print("means: {}, {}".format(ftpl_mean, br_mean))
-    file.write("X: {}".format(X))
-    file.write("Y: {}".format(Y))
-    file.write("BR: {}".format(BR))
 
-    file.write("X_A: {}".format(XAs))
-    file.write("X_B: {}".format(XBs))
-    file.close()
+    file_BR.close()
+    file_Y.close()
     return X, np.array(Y)-np.array(BR)
 
 def main():
-    Xs, Ys = [], []
-
-    Xs.append(X)
-    Ys.append(Y)
-    X_mean = np.mean(Xs, axis=0)
-    Y_mean = np.mean(Ys, axis=0)
-    print(X_mean, Y_mean)
+    run(3)
+    for i in range(0, 15):
+        run(i)
+    # Xs, Ys = [], []
+    # X, Y = run(3)
+    # Xs.append(X)
+    # Ys.append(Y)
+    # X_mean = np.mean(Xs, axis=0)
+    # Y_mean = np.mean(Ys, axis=0)
+    # print(X_mean, Y_mean)
     # plt.scatter(X, Y)
     # plt.show()
     # plt.savefig('ftpl.png', dpi=300)
